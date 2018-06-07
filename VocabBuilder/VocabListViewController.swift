565537d412b4016e86fb5f922f0b6cf006dd261d
Tomas Leriche <tomasleriche@tomass-mbp>
2018-06-07 17:58:24 +1200

    basic table done with select and deselect accessory checkmark

---
+++ VocabBuilder/VocabListViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  VocabBuilder
+//
+//  Created by Tomas Leriche on 6/7/18.
+//  Copyright © 2018 Tomas Leriche. All rights reserved.
+//
+
+import UIKit
+
+class VocabListViewController: UITableViewController {
+    
+    let itemArray = ["Word1", "Word2", "Word3"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+        }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        print(indexPath.row)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
